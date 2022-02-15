aa64585f1e83c83b14c6c7eadddb749e325804b8
Andrey Shlyapnikov <48034349+usernaimandrey@users.noreply.github.com>
2022-02-16 02:20:39 +0300

    Update Request.js

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export  function request(text) {
-    const response =  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}+inauthor:keyes&key=AIzaSyAIH6MET0pwiG0rPsy4dGoNllnz7UTvhAI`);
+    const response =  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}`);
          response.then((data) => console.log(data.data));
          
 }
