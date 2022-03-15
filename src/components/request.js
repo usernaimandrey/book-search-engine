ef9f5d96469c05138e063a6a1dd1faad0c28d985
usernaimandrey <anshlyapnikov@yandex.ru>
2022-03-15 17:45:51 +0300

    fix linter config

--- src/components/request.js
+++ src/components/request.js
@@ -3,7 +3,7 @@ import getRout from '../routes.js';
 
 const request = async (text, categorie, oredrBy) => {
     const url = new URL(getRout());
-    const subject = categorie === 'all' ? null : `+subject:${categorie}`;
+    const subject = categorie === 'all' ? '' : `+subject:${categorie}`;
     url.searchParams.append('q', `${text}${subject}`);
     url.searchParams.append('orderBy', oredrBy);
     url.searchParams.append('key', process.env.REACT_APP_API_KEY);
